2a385785f470063d12a97f94d4124ef9f652c204
VillaAlexTor <alexvillarroeltorrico@gmail.com>
2025-11-09 17:32:28 -0400

    Cambio: Main.jsx 1

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import AuthProvider from "./context/AuthContext.jsx";
-import { HashRouter as Router } from "react-router-dom";
 import "./styles/tailwind.css";
 import "./styles/globals.css";
 
     ReactDOM.createRoot(document.getElementById("app")).render(
         <React.StrictMode>
-            <Router>
             <AuthProvider>
                 <App />
             </AuthProvider>
-            </Router>
         </React.StrictMode>
     );
