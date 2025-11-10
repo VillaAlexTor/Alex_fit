b04ec48cca906218a5ff78d939499e03b23a56d2
VillaAlexTor <alexvillarroeltorrico@gmail.com>
2025-11-09 21:59:01 -0400

    Cambio: cerrando secion con codigo 1

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -5,6 +5,3 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-await supabase.auth.signOut();
-localStorage.clear();
-location.reload();
