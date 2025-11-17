406c668ffc68dddfb4acf0c6f0ded10831ddd155
VillaAlexTor <alexvillarroeltorrico@gmail.com>
2025-11-17 09:50:12 -0400

    Cambio

--- src/pages/Dashboard/Rutina.jsx
+++ src/pages/Dashboard/Rutina.jsx
@@ -1,4 +1,4 @@
-// Alex_fit/src/pages/Das hboard/Rutina.jsx
+// Alex_fit/src/pages/Dasboard/Rutina.jsx
 import React, { useEffect, useState, useContext } from "react";
 import { supabase } from "../../utils/supabaseClient";
 import { AuthContext } from "../../context/AuthContext.jsx";
